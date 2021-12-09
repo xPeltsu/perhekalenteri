21866c919abfda204176867efd8e5234c9174ee5
Esa Pelander <esa.pelander@edu.sasky.fi>
2021-12-09 09:58:44 +0200

    uusi otsikkofontti lisätty

--- globals.php
+++ globals.php
@@ -25,7 +25,10 @@
       "url"  => "https://fonts.googleapis.com/css2?family=Sigmar+One&display=swap"),
     "titanone" => array(
       "name" => "Titan One",
-      "url"  => "https://fonts.googleapis.com/css2?family=Titan+One&display=swap")
+      "url"  => "https://fonts.googleapis.com/css2?family=Titan+One&display=swap"),
+    "permanentmarker" => array(
+       "name" => "Permanent Marker",
+       "url"  => 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap')
   );
 
   // Otsakkeen taustat aakkosjärjestyksessä.
